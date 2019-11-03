5cc1f5766eb9cbab2367334a8fe34a548e5fccc8
Mikhailiandr <43167372+Mikhailiandr@users.noreply.github.com>
2019-11-03 14:06:13 +0300

    Revert "ButtomNavigationBar(Menu)"
    This reverts commit 26e166616d30bfa9f1dd9ae972ae3d29e8a76be8.

--- app/src/main/java/com/example/autoserviceapp/HomeActivity.java
+++ app/src/main/java/com/example/autoserviceapp/HomeActivity.java
@@ -1,13 +1,8 @@
 package com.example.autoserviceapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,22 +10,5 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.btm_nav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                int id = menuItem.getItemId();
-
-                if (id == R.id.profile){
-                    ProfileFragment fragment = new ProfileFragment();
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.frame_layout,fragment);
-                    fragmentTransaction.commit();
-                }
-                return true;
-            }
-        });
-        bottomNavigationView.setSelectedItemId(R.id.main);
     }
 }
