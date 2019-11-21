fb2a47465eb209be278726adc137898872c0881f
Dmitry <43620456+JavaOneLove@users.noreply.github.com>
2019-11-21 19:21:20 +0300

    Menu

--- app/src/main/java/com/example/autoserviceapp/HomeActivity.java
+++ app/src/main/java/com/example/autoserviceapp/HomeActivity.java
@@ -6,20 +6,34 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public int role = 0; // 0 is no auth, 1 is auth, 2 is manager, 3 is administrator
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.btm_nav);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        switch (role){
+            case (0):
+                bottomNavigationView.inflateMenu(R.menu.menu_bottom_no_auth);
+                break;
+            case(1):
+                bottomNavigationView.inflateMenu(R.menu.menu_bottom_auth);
+                break;
+            case(2):
+                bottomNavigationView.inflateMenu(R.menu.menu_bottom_admin);
+                break;
+            case (3):
+                bottomNavigationView.inflateMenu(R.menu.menu_bottom_manager);
+                break;
+        }
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 int id = menuItem.getItemId();
