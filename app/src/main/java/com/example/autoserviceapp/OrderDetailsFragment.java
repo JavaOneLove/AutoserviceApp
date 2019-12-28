65dc57d2c532cc27378e9a3edc75c24b87a3fea6
Dmitry <43620456+JavaOneLove@users.noreply.github.com>
2019-12-28 14:36:34 +0300

    SmallFix

--- app/src/main/java/com/example/autoserviceapp/OrderDetailsFragment.java
+++ app/src/main/java/com/example/autoserviceapp/OrderDetailsFragment.java
@@ -1,31 +1,59 @@
 package com.example.autoserviceapp;
 
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class OrderDetailsFragment extends Fragment {
-
-
-    public OrderDetailsFragment() {
-        // Required empty public constructor
-    }
+    TextView currentDateTime;
+    Calendar dateAndTime=Calendar.getInstance();
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_order_details, container, false);
+        currentDateTime = view.findViewById(R.id.currentDateTime);
+        setInitialDateTime();
+        return view;
     }
+
+    public void setDate(View v) {
+        new DatePickerDialog(getActivity(), d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+    private void setInitialDateTime() {
+
+        currentDateTime.setText(DateUtils.formatDateTime(getActivity(),
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+                        | DateUtils.FORMAT_SHOW_TIME));
+    }
+
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
 
 }
